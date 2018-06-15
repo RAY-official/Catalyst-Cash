31ad84cd23950f05238cb50060598c2f66d7999e
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2018-06-15 14:12:29 +0900

    ok

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -180,8 +180,8 @@ const std::vector<uint64_t> Currency::POWERS_OF_TEN = {
                     baseReward = bad_tail_emission_reward;
                  }
                  }
-                if (height >= 500000) {
-                if (alreadyGeneratedCoins + CryptoNote::parameters::TAIL_EMISSION_REWARD >= m_moneySupply || baseReward <                   CryptoNote::parameters::TAIL_EMISSION_REWARD)
+                if (height >= 300000) {
+                if (alreadyGeneratedCoins + CryptoNote::parameters::TAIL_EMISSION_REWARD <= m_moneySupply || baseReward <                   CryptoNote::parameters::TAIL_EMISSION_REWARD)
 		{
 		     baseReward = CryptoNote::parameters::TAIL_EMISSION_REWARD;
                  }
