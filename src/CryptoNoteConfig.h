4794f08f74503e39df1860027398152b55b545f9
RAY-official <project.ray.coin.r.a.yofficial@gmail.com>
2018-06-16 20:45:09 +0300

    put github

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -73,7 +73,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "PhotonCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001c9faeb38029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210180cf48704271144d7bdcc6b63369a37903e55c372cdcf44a141db891a11e0a94";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
